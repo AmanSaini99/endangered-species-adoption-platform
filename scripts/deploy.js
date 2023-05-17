d69f28f50e4b57dee42e4ac6fa3e9b284bac877b
AmanSaini99 <amansaini120799@gmail.com>
2023-05-17 11:05:33 +0530

    final project

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,19 +5,22 @@ async function main() {
     console.log('Deploying contracts with the account: ' + deployer.address);
 
     // Deploy First
-    const First = await hre.ethers.getContractFactory('EndangeredSpeciesToken');
+    const First = await hre.ethers.getContractFactory('Wild');
     const first = await First.deploy();
 
-
-    const Second = await hre.ethers.getContractFactory('EndangeredSpeciesDapp');
+    const Second = await hre.ethers.getContractFactory('DonationNFT');
     const second = await Second.deploy();
 
-    const Third = await hre.ethers.getContractFactory('GuardianshipNFT');
+    const Third = await hre.ethers.getContractFactory('GaurdianshipNFT');
     const third = await Third.deploy();
+
+    const Main = await re.ethers.getContractFactory('Controller');
+    const main = await Third.deploy(first.address, second.address, third.address);
 
     console.log( "First: " + first.address );
     console.log( "Second: " + second.address );
-    // console.log( "Second: " + third.address );
+    console.log( "Third: " + third.address );
+    console.log( "Main: " + main.address );
 }
 
 main()
